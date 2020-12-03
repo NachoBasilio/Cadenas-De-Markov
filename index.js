70cb245d5d3cf8ed20bc89ecc40b4e591ec9c46c
Ignacio Basilio <arcortoon@gmail.com>
2020-12-03 20:14:21 -0300

    Termine aunuqe no da tan certero.

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const separadora = (PartidosJugados) => {
     let CantidadDeWL = 0
     let CantidadDeLW = 0
     let CantidadDeLL = 0
+    let CantidadDeW = 0
+    let CantidadDeL = 0
 
     for(let i = 0; i < PartidosJugados.length; i++){
        switch(PartidosJugados[i]+PartidosJugados[i+1]){
@@ -23,7 +25,7 @@ const separadora = (PartidosJugados) => {
            break
            case "WL" : CantidadDeWL = CantidadDeWL + 1 
            break
-           case "LW" : CantidadDeLW = CantidadDeLW + 1
+           case "LW": CantidadDeLW = CantidadDeLW +1
            break
            case "LL" : CantidadDeLL = CantidadDeLL + 1
            break
@@ -32,15 +34,34 @@ const separadora = (PartidosJugados) => {
             break
             case "WL" : CantidadDeWL = CantidadDeWL + 1 
             break
-            case "LW" : CantidadDeLW = CantidadDeLW + 1
+            case "LW": CantidadDeLW = CantidadDeLW +1
             break
             case "LL" : CantidadDeLL = CantidadDeLL + 1
             break
            }
            break
        }
+
     }
-    return [CantidadDeLW, CantidadDeWW, CantidadDeLL, CantidadDeWL]
+    for(let i = 0; i < PartidosJugados.length; i++){
+        if(PartidosJugados[i] === "W"){
+            CantidadDeW = CantidadDeW + 1
+        }else{
+            CantidadDeL = CantidadDeL + 1
+        }
+    }
+    let ProbabilidadDeWW = (CantidadDeWW/PartidosJugados.length)
+    let ProbabilidadDeWL = (CantidadDeWL/PartidosJugados.length)
+    let ProbabilidadDeLW = (CantidadDeLW/PartidosJugados.length)
+    let ProbabilidadDeLL = (CantidadDeLL/PartidosJugados.length)
+    let ProbabilidadDeW  = (CantidadDeW/PartidosJugados.length)
+    let ProbabilidadDeL  = (CantidadDeL/PartidosJugados.length)
+    let WW = ProbabilidadDeWW/ProbabilidadDeW
+    let LW = ProbabilidadDeWL/ProbabilidadDeW
+    let WL = ProbabilidadDeLW/ProbabilidadDeL
+    let LL = ProbabilidadDeLL/ProbabilidadDeL
+
+    return [`P(W|W) ${WW}`,`P(W|L) ${WL}`,`P(L|W) ${LW}`,`P(L|L) ${LL}`, WW + LW, LL + WL ]
 }
 
 
